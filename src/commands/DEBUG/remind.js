0803c04c1d0a8d42c2e720041cac19026a9a966f
MarshaIIs <55185108+MarshaIIs@users.noreply.github.com>
2021-02-18 18:52:00 +0100

    Poke command & Server-side fix
    Moved the Remind command to DEBUG to avoid the PI from crashing
    
    Fixed Poke command to only work in certain channels

--- src/commands/DEBUG/remind.js
+++ src/commands/DEBUG/remind.js
@@ -1,67 +1,59 @@
-const commando = require('discord.js-commando');
-const moment = require('moment');
+//const db = require('quick.db');
+
+function channelCheck(message) {
+    /* 687306978443132958 = #bot-spam
+     * 624299862464135170 = #voicechat
+     * 634050395336998933 = #cmd-testing
+     */
+
+    if(!(message.channel == '687306978443132958' || message.channel == '624299862464135170' || message.channel == '634050395336998933')) {
+        message.reply('please use a correct channel, such as <#687306978443132958>').then(msg => {
+            msg.delete({ timeout: 30000 })
+        })
+        .catch(console.error);
+        
+        return false;
+    }
+}
 
 module.exports = {
     name: 'remindme',
-    decription: 'Reminder command',
-
+    decription: 'This command will send the user a message after X amount of time (in minutes).',
+    type: 'general',
+    usage: "remindme <TIME>",
+    
     async run (client, message, args) {
-        console.log('remind args ' + args);
-        console.log('remind message ' + message);
+        if(channelCheck(message) == false) return;
 
-        //if (args.length === 0) return message.reply('provide an interval for the reminder');
-        
-        message.channel.send('Specify time interval for reminder')
-        
-        // await ; {
-        //     console.log('Reminder Mesage Content time: ' + message.content);
-        // }
-        
-        //message.channel.send('Specify meesage')
+        if(!message.member.hasPermission('ADMINISTRATOR') && member.roles.cache.some(role => role.name === 'elite')) {
+            console.log(`${message.author.tag}` + ' is missing permissions to perform \"' + this.name + '\" command');
+            perm_msg = new Discord.MessageEmbed()
+                .setTitle('Error!')
+                .setColor('db0606')
+                .setDescription('You do not have permission to use that command!');
 
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        // const splitMessage = args.join(' ');
-        // const filteredMessage = args.replace(splitMessage[0], '');
-
-        // console.log('Remindme args: ' + args)
-        // function reminder() {
-        // message.reply("\n**REMINDER:**\n" + filteredMessage);
-        // }
-
-        // switch(splitMessage[0].slice(-1)) {
-        //     case 's': {
-        //         var msDelay = splitMessage[0].slice(0, -1) * 1000;
-        //         message.reply("Your reminder has been set. I will remind you in " + splitMessage[0].slice(0, -1) + "seconds.");
-        //         setTimeout(reminder, msDelay);
-        //         break;
-        //     }
-        //     case 'm': {
-        //         var msDelay = splitMessage[0].slice(0, -1) * 60000;
-        //         message.reply("Your reminder has been set. I will remind you in " + splitMessage[0].slice(0, -1) + "minutes.");
-        //         setTimeout(reminder, msDelay);
-        //         break;
-        //     }
-        //     case 'h': {
-        //         var msDelay = splitMessage[0].slice(0, -1) * 3600000;
-        //         message.reply("Your reminder has been set. I will remind you in " + splitMessage[0].slice(0, -1) + "hours.");
-        //         setTimeout(reminder, msDelay);
-        //         break;
-        //     }
-        //     case 'd': {
-        //         var msDelay = splitMessage[0].slice(0, -1) * 86400000;
-        //         message.reply("Your reminder has been set. I will remind you in " + splitMessage[0].slice(0, -1) + "days.");
-        //         setTimeout(reminder, msDelay);
-        //         break;
-        //     }
-        // }
+            return message.channel.send(perm_msg);
         }
+
+        let remindMin = args.join(" ");
+        remindSec = parseInt(remindMin, 10) * 60;
+        remindMS = (remindSec * 1000)
+        console.log('Reminder set to ' + remindSec + ' seconds...');
+        
+        if      (!remindSec) return message.reply('the proper usage is ' + process.env.PREFIX + 'remindme <NUMBER>.');
+        else if ((remindSec) > 3600) return message.reply('your reminder cannot be longer than 1 hour');
+        else if ((remindSec) <= 59) return message.reply('your reminder must be atleast 1 minute long.');
+        else {
+            message.channel.send("-- REMINDER --")
+            
+            let setReminder = await db.fetch(`daily_${message.author.id}`);
+            if (setReminder !== null && cooldown - (Date.now() - setReminder) > 0) {
+
+                // If user still has a cooldown
+                let timeObj = ms(remindMS - (Date.now() - setReminder)); // timeObj.hours = 12
+            } else {
+                // Otherwise they'll get their daily
+            }
+        }
+    }
 }
--- src/commands/poke.js
+++ src/commands/poke.js
@@ -2,6 +2,22 @@ const Discord = require('discord.js')
 const GphApiClient  = require('giphy-js-sdk-core');
 giphy = GphApiClient(process.env.GIPHY_TOKEN)
 
+function channelCheck(message) {
+    /* 687306978443132958 = #bot-spam
+     * 624299862464135170 = #voicechat
+     * 634050395336998933 = #cmd-testing
+     */
+
+    if(!(message.channel == '687306978443132958' || message.channel == '624299862464135170' || message.channel == '634050395336998933')) {
+        message.reply('please use a correct channel, such as <#687306978443132958>').then(msg => {
+            msg.delete({ timeout: 30000 })
+        })
+        .catch(console.error);
+        
+        return false;
+    }
+}
+
 module.exports = {
     name: 'poke',
     decription: 'When a user is mentioned with the poke command a private DM is sent to them.',
@@ -10,6 +26,7 @@ module.exports = {
     usage: "<MENTION>",
     
     async run (client, message, args) {
+        if(channelCheck(message) == false) return;
         if (!message.mentions.members.first() || message.mentions.has(client.user)) return message.reply('the proper usage is ' + process.env.PREFIX + 'poke <USER MENTION>');
 
         else {
